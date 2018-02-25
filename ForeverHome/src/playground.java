658ea887592b73b79525890e028b7646cb414220
TheBrain <TheBrain@MSI>
2018-02-25 15:19:05 -0500

    First Commit

---
+++ ForeverHome/src/playground.java
@@ -0,0 +1,11 @@
+
+public class playground {
+
+	public static void main(String[] args) {
+		System.out.println("Line edited by Brian Ruthrauff");
+		System.out.println("Line edited by <name>");
+		System.out.println("Line edited by <name>");
+		System.out.println("Line edited by <name>");
+	}
+
+}
