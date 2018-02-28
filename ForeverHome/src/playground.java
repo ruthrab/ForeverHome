811b113b00b93779c52d3a629dc83e52feed0a43
DR37 <DAIR@MIAMIOH.EDU>
2018-02-28 12:18:22 -0500

    Update playground.java
    added

--- ForeverHome/src/playground.java
+++ ForeverHome/src/playground.java
@@ -5,7 +5,7 @@ public class playground {
 		System.out.println("Line edited by Brian Ruthrauff-3");
 		System.out.println("Line edited by Bailey Ralston-2");
 		System.out.println("Line edited by Binfang Ye");
-		System.out.println("Line edited by <name>");
+		System.out.println("Line edited by Rui Dai");
 	}
 
 }
