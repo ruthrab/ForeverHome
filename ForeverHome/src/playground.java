d14136764376bc4038025fdf2b9a09c41b0e1bbf
yebin <yebin@172.25.120.244>
2018-02-28 12:15:02 -0500

    edit Binfang Ye

--- ForeverHome/src/playground.java
+++ ForeverHome/src/playground.java
@@ -4,7 +4,7 @@ public class playground {
 	public static void main(String[] args) {
 		System.out.println("Line edited by Brian Ruthrauff-3");
 		System.out.println("Line edited by Bailey Ralston-2");
-		System.out.println("Line edited by <name>");
+		System.out.println("Line edited by Binfang Ye");
 		System.out.println("Line edited by <name>");
 	}
 
