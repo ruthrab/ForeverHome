059faf421650b7928507956e4889fa6f9a8a65f1
Bailey Ralston <ralstobj@miamioh.edu>
2018-04-01 00:13:31 -0400

    Created user, shelter, pet, and session api as wells as adding the homepage html and css.

---
+++ ForeverHome/webapp/fureverhome/api/controllers/ShelterController.js
@@ -0,0 +1,11 @@
+/**
+ * ShelterController
+ *
+ * @description :: Server-side logic for managing shelters
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
