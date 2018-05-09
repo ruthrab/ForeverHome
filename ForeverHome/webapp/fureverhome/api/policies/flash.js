81c8da5227c8fc66a641671bfc5d6a9facb7b468
Bailey Ralston <ralstobj@miamioh.edu>
2018-05-09 00:39:59 -0400

    added sanitation and additional use cases

--- ForeverHome/webapp/fureverhome/api/policies/flash.js
+++ ForeverHome/webapp/fureverhome/api/policies/flash.js
@@ -1,13 +1,33 @@
 module.exports = function (req, res, next) {
-
+  if(req.session.User !=undefined){
+    if(req.session.User.isShelter){
+      if(req.session.Shelter == undefined){
+      }
+    }
+  }
   res.locals.flash = {};
 
-  if (!req.session.flash) return next();
+  if (!req.session.flash){
+    if(req.session.User !=undefined){
+      if(req.session.User.isShelter){
+        if(req.session.Shelter == undefined){
+          return res.redirect('shelter/new');
+        }
+      }
+    }
+    return next();
+  } 
 
   res.locals.flash = _.clone(req.session.flash);
 
   // clear flash
   req.session.flash = {};
-
+  if(req.session.User !=undefined){
+    if(req.session.User.isShelter){
+      if(req.session.Shelter == undefined){
+        return res.redirect('shelter/new');
+      }
+    }
+  }
   next();
 };
